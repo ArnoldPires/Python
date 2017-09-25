0afd2fc6b193213600cbfe1ce46ecefc12d5c6c3
ArnoldPires <arnold.pires92@gmail.com>
2017-09-25 17:56:18 -0400

    added hello.py file

---
+++ testing-stuff/Hello.py
@@ -0,0 +1,31 @@
+import random
+import sys
+import os
+
+'''
+The 5 main types in Python are:
+1. Numbers
+2. Strings
+3. Lists
+4. Tuples
+5. Dictionaries
+'''
+# How to write just plain text:
+print("Hello World")
+# This is just a random comment used to write down a single line
+'''
+This is a multi-line comment I guess.
+Hello, another comment! YAYAYAYAYAY
+HISDHFOUDSBONWONEV SIFDSOJNVQEOV  EBVSDJBFKHBFEQUBC J
+'''
+# example of a variable:
+name = "Arnold"
+print(name)
+name = 15
+#math
+print("67 + 30 =" , 67+30)
+print("700 + 209867326485324 =", 700+209867326485324)
+print("Always remember that in order to get better at something, you must commite the time to learn and get better at it")
+#This is how you add in spaces in the terminal:
+print('\n' * 4)
+print("This is some more text, only this time there are more spaces above")
